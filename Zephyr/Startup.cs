45577f160ca56528b665feb35a0313a6ec9386db
Brent <gkdog@bellsouth.net>
2015-04-21 14:02:54 -0400

    creation

---
+++ Zephyr/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zephyr.Startup))]
+namespace Zephyr
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
